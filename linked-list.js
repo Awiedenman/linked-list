0b7d860dc9db54048c06481879a7ff936a4b9444
Austin.Wiedenman <awiedenman@gmail.com>
2017-12-12 18:23:59 -0700

    End of the day Tues

--- linked-list.js
+++ linked-list.js
@@ -1,16 +1,15 @@
 var parent = $("#section-2");
 
 $('#enter-button').on('click', createCard);
-$('#enter-button').on('click', deleteEntry)
+$('#enter-button').on('click', deleteEntry);
 
+// $('#section-2').on('click','#website-list-delete-button', removeBookmarkFromList);
 
 function createCard(){
   event.preventDefault();
-  console.log("hello")
   var websiteTitleInput =$("#website-title-input").val();
   var websiteUrlInput =$("#website-url-input").val();
   parent.prepend(`
-    <ul id="website-list">
     <li>
       <section class= list-title-box>
         <h2 id="website-list-title">${websiteTitleInput}</h2>
@@ -22,13 +21,31 @@ function createCard(){
         <button id="website-list-read-button">Read</button>
         <button id="website-list-delete-button">Delete</button>
       </section>
-  </ul>
+    </li>  
 `);
 }
 
 function deleteEntry() {
-
+$('#website-title-input').val('');
+$('#website-url-input').val('');
 }
+
+
+
+// function removeBookmarkFromList() {
+  // console.log(event, "function");
+  // console.log(1);
+  // if(event.target.id === 'website-list-delete-button') {
+    
+  //   console.log('delete button');
+  // } else {
+  //   console.log('section-2');
+  // } 
+  $('#section-2').on('click','#website-list-delete-button', function() { 
+    $(this).closest('li').remove('.list-title-box');
+ });  
+
+
  
      
         
